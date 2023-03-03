f4152e586b58a6c4362591b6bb34974750f334f4
Gaby M <gmarin1997@gmail.com>
2023-03-03 15:34:51 -0600

    [patch]: fixing netlify url redirect issue

--- github-finder/src/context/github/GithubActions.js
+++ github-finder/src/context/github/GithubActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const GITHUB_URL =
-	process.env.REACT_APP_GITHUB_URL;
+	'https://api.github.com';
 const GITHUB_TOKEN =
-	process.env.REACT_APP_GITHUB_TOKEN;
+	process?.env?.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
 	baseURL: GITHUB_URL,
